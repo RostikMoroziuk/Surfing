f304cfce854fe979d55a8be5453cd5fb47f3d9ba
RostikMoroziuk <rmorozyukua@gmail.com>
2017-05-13 01:12:56 +0300

    Add adaptive carousel to shop section

--- js/script.js
+++ js/script.js
@@ -4,25 +4,39 @@ $(window).resize(resizeControll);
 function resizeControll() {
     var nav = document.getElementsByClassName("navbar")[0];
 
-    if ($(window).width() <= 767)
-    {       
+    if ($(window).width() <= 767) {
         nav.className = "navbar navbar-default";
-    }
-    else {
+    } else {
         nav.className = "navbar";
     }
 }
 
 $('.carousel').carousel({
-  interval: 6500
+    interval: 6500
 })
 
 
 //slick slider
 $('.boards').slick({
-  lazyLoad: 'ondemand',
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: false,
-  autoplaySpeed: 2000
+    lazyLoad: 'ondemand',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    centerMode: true,
+    centerPadding: '0px',
+    responsive: [{
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
